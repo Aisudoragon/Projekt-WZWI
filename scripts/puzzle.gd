1a5be95243a881955bc24d2244ad432eaef9c6e1
Aisudoragon <krybex101@gmail.com>
2024-01-30 19:34:07 +0100

    Ulozenie kostek do ulozenia oraz dodanie markerow do sledzenia gdzie ma byc sasiad

--- scripts/puzzle.gd
+++ scripts/puzzle.gd
@@ -3,6 +3,7 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# Ułożenie klocków losowo wokół pierwszego elementu
 	var tekst := "Piece"
 	for i in 9:
 		$Path3D/PathFollow3D.set_progress_ratio(randf())
@@ -13,7 +14,9 @@ func _ready():
 				randf_range(0, 6.28),
 				randf_range(0, 6.28)))
 
+	# TODO: Snapowanie do markera. Odgległość snapowania <0.05
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	pass
+	print($Piece1/Marker3D.get_position().distance_to($Piece2.get_position()))
