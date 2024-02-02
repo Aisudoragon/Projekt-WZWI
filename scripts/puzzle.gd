115368c763055178d74e9c091a3942152ffb079e
Aisudoragon <krybex101@gmail.com>
2024-02-02 23:47:11 +0100

    Finalizacja projektu

--- scripts/puzzle.gd
+++ scripts/puzzle.gd
@@ -4,7 +4,7 @@ extends Node3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Ułożenie klocków losowo wokół pierwszego elementu
-	for i in 8:
+	for i in 7:
 		$Path3D/PathFollow3D.set_progress_ratio(randf())
 		var obiekt := get_node("Piece" + str(i + 1))
 		obiekt.set_position($Path3D/PathFollow3D.get_position())
@@ -17,7 +17,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	# Teleportuj klocek na stół po spadnięciu na podłogę
-	for i in 8:
+	for i in 7:
 		var obiekt := get_node("Piece" + str(i + 1))
 		if obiekt.global_position.y <= 0.25:
 			obiekt.set_position(Vector3(
@@ -28,6 +28,12 @@ func _process(_delta):
 		if (
 				obiekt.get_global_position().distance_to(marker.get_global_position()) <= 0.02
 				and obiekt.is_freeze_enabled() == not true
+				and obiekt.rotation.x - marker.rotation.x <= 0.05
+				and obiekt.rotation.x - marker.rotation.x >= -0.05
+				and obiekt.rotation.y - marker.rotation.y <= 0.05
+				and obiekt.rotation.y - marker.rotation.y >= -0.05
+				and obiekt.rotation.z - marker.rotation.z <= 0.05
+				and obiekt.rotation.z - marker.rotation.z >= -0.05
 		):
 			print("ELEMENT NA MIEJSCU")
 			obiekt.set_freeze_enabled(true)
